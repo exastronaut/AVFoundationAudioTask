e562fe5fefff9249fa455744c9808fc6c131101a
Artem Sviridov <exastronaut@yandex.ru>
2022-09-30 09:48:56 +0300

    added project

---
+++ AVFoundation_Audio/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  AVFoundation_Audio
+//
+//  Created by Niki Pavlove on 18.02.2021.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
+
